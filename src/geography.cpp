56dbad8029960fc3758839734feb65ba04a76b00
Michael Bradley <michaelmbrad@gmail.com>
2024-03-11 14:44:35 -0400

    Measure Grid randomization speed

--- src/geography.cpp
+++ src/geography.cpp
@@ -1,6 +1,7 @@
 #include "geography.h"
 
 #include <algorithm>
+#include <chrono>
 #include <iostream>
 
 #include "constants.h"
@@ -18,10 +19,17 @@ Geography::Geography() {
 }
 
 void Geography::Randomize() {
+  using std::chrono::duration_cast;
+  using std::chrono::milliseconds;
+
   height_ = Grid();
+  auto start_time = chrono::high_resolution_clock::now();
   for (size_t i = 0; (kDetail >> i) > kMinDetail; ++i) {
     cout << "Generating Perlin noise with major nodes every " << (kDetail >> i)
          << " values\n";
     height_ += Grid::PerlinNoise(kDetail >> i) / (1 << i);
   }
+  auto end_time = chrono::high_resolution_clock::now();
+  cout << "Generation time: "
+       << duration_cast<milliseconds>(end_time - start_time).count() << "ms\n";
 }
